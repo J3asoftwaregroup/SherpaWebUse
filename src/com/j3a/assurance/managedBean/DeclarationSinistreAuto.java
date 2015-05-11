acd73845313512a43041f1aa6bd8323b3cf0d612
PCALekerand <kerandelniangoran@gmail.com>
2015-05-11 12:27:42 +0000

    Page index

--- src/com/j3a/assurance/managedBean/DeclarationSinistreAuto.java
+++ src/com/j3a/assurance/managedBean/DeclarationSinistreAuto.java
@@ -1,5 +1,7 @@
 package com.j3a.assurance.managedBean;
 
+import java.math.BigDecimal;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.j3a.assurance.model.Contrat;
+import com.j3a.assurance.model.Sinistre;
 import com.j3a.assurance.objetService.ObjectService;
 
 @Component
@@ -21,6 +24,15 @@ public class DeclarationSinistreAuto {
 	private String optionRech ="1";
 	private String valeurDeRecherche;
 	private Contrat monContrat = new Contrat();
+	private String lieu;
+	private String domageCorps;
+	private String domageMat;
+	private int nbreBlesse;
+	private int nbreDeces;
+	private BigDecimal rcAssure=new BigDecimal(0);
+	private Sinistre sinistre = new Sinistre();
+	
+	
 	
 	
 	public String onFlowProcess(FlowEvent event) {
@@ -55,20 +67,10 @@ public class DeclarationSinistreAuto {
 	}
 
 
-
-
-
-
-
 /**********************ACCESSEURS***************************/
 	public String getOptionRech() {
 		return optionRech;
 	}
-
-
-
-
-
 
 
 
@@ -77,19 +79,9 @@ public class DeclarationSinistreAuto {
 	}
 
 
-
-
-
-
-
 	public String getValeurDeRecherche() {
 		return valeurDeRecherche;
 	}
-
-
-
-
-
 
 
 	public void setValeurDeRecherche(String valeurDeRecherche) {
@@ -118,6 +110,66 @@ public class DeclarationSinistreAuto {
 
 	public void setMonContrat(Contrat monContrat) {
 		this.monContrat = monContrat;
+	}
+
+
+	public String getLieu() {
+		return lieu;
+	}
+
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+
+
+	public Sinistre getSinistre() {
+		return sinistre;
+	}
+
+
+	public void setSinistre(Sinistre sinistre) {
+		this.sinistre = sinistre;
+	}
+
+
+	public String getDomageCorps() {
+		return domageCorps;
+	}
+
+
+	public void setDomageCorps(String domageCorps) {
+		this.domageCorps = domageCorps;
+	}
+
+
+	public String getDomageMat() {
+		return domageMat;
+	}
+
+
+	public void setDomageMat(String domageMat) {
+		this.domageMat = domageMat;
+	}
+
+
+	public int getNbreBlesse() {
+		return nbreBlesse;
+	}
+
+
+	public void setNbreBlesse(int nbreBlesse) {
+		this.nbreBlesse = nbreBlesse;
+	}
+
+
+	public int getNbreDeces() {
+		return nbreDeces;
+	}
+
+
+	public void setNbreDeces(int nbreDeces) {
+		this.nbreDeces = nbreDeces;
 	}
 
 }
