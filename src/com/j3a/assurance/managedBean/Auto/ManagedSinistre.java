04e242b60a8bfe5b806340c8de4026074b12f9c3
charlyj3a <charly85>
2015-05-27 19:45:43 +0000

    Comitt

--- src/com/j3a/assurance/managedBean/Auto/ManagedSinistre.java
+++ src/com/j3a/assurance/managedBean/Auto/ManagedSinistre.java
@@ -1,5 +1,11 @@
 package com.j3a.assurance.managedBean.Auto;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -8,15 +14,25 @@ import java.util.Date;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.FlowEvent;
+import org.primefaces.model.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.j3a.assurance.model.ActeMedical;
 import com.j3a.assurance.model.Avenant;
 import com.j3a.assurance.model.Conducteur;
 import com.j3a.assurance.model.ConducteurSinistre;
@@ -24,6 +40,9 @@ import com.j3a.assurance.model.ConduireVehicule;
 import com.j3a.assurance.model.Contrat;
 import com.j3a.assurance.model.Facture;
 import com.j3a.assurance.model.Garantie;
+import com.j3a.assurance.model.Intervenant;
+import com.j3a.assurance.model.Intervention;
+import com.j3a.assurance.model.Medecin;
 import com.j3a.assurance.model.Personne;
 import com.j3a.assurance.model.Sinistre;
 import com.j3a.assurance.model.SinistreConducteur;
@@ -35,6 +54,8 @@ import com.j3a.assurance.model.VehiculesAssures;
 import com.j3a.assurance.objetService.ObjectService;
 import com.j3a.assurance.utilitaire.ContratResum;
 import com.j3a.assurance.utilitaire.IdGenerateur;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Font;
 
 @Component
 public class ManagedSinistre implements Serializable {
@@ -62,17 +83,43 @@ public class ManagedSinistre implements Serializable {
 	private List<ContratResum> listContratVehicule = new ArrayList<>();
 	private ContratResum selectedContratResum;
 	private ContratResum selectedContratResumVeh = new ContratResum();
+	private Intervention intervention = new Intervention();
+	private Intervenant intervenant = new Intervenant();
+	private ActeMedical acteMedical = new ActeMedical();
+	private Medecin medecin = new Medecin();
+	private Facture facAct = new Facture();
+	private Facture facPv = new Facture();
 	private List<Sinistre> listSinistreEnCours = new ArrayList<Sinistre>();
 	private Date dateDeclaration = Calendar.getInstance().getTime();
-	
+	private boolean bPv, bRap, bActMed;
+	private boolean etatPv=true, etatRap=true, etatActMed=true;
 	private String numPolice = "";
 	private String codeVehicule;
 	private Facture facture;
     private String libelleDomageCorp;
 	private String typeFiltre;
 	private String filtre;
+	private String idIntervenant, idMedecin;
+	private List<SelectItem> elementsNomInt;
+	private List<SelectItem> elementsNomMed;
+	private UploadedFile filePv;
+	private UploadedFile fileRap;
+	private UploadedFile fileActMed;
+	private UIComponent buttonadd;
+	private static String FILE = "c:/upload/temp/default.pdf";
+	private String destination = "c:\\upload\\temp\\";
+	private java.lang.String typeRapport;
+	private String nomfichier;
    private BigDecimal rcAssure=new BigDecimal(0);
-	
+   private static final int DEFAULT_BUFFER_SIZE = 10240000;
+   private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
+			Font.BOLD);
+	private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+			Font.NORMAL, BaseColor.RED);
+	private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
+			Font.BOLD);
+	private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+			Font.BOLD);
 
 	
 		Boolean etatRecherche = false;
@@ -349,7 +396,355 @@ public class ManagedSinistre implements Serializable {
 				return resultatPersonne;
 		}
 	
+	//CheckBox En fonction des choix
+	public void ChangeValueChexBox(){
+		//bouton PV
+		if (isbPv()==true ){
+			setEtatPv(false);
+		}
+		else{
+			setEtatPv(true);
+		}
+		 //bouton rapport
+		if (isbRap()==true){
+			setEtatRap(false);
+		}
+		else{
+			setEtatRap(true);
+		}
+		
+		//Bouton Acte
+		if(isbActMed()==true){
+			setEtatActMed(false);
+		}
+		else{
+			setEtatActMed(true);
+		}
+	}
+	
+	//Recup l'intervenant
+			public void RecupInt() {
+				setIntervenant((Intervenant) getObjectService().getObjectById(
+						idIntervenant, "Intervenant"));
+			}
+			
+	//Recup Medecin
+			public void RecupMed() {
+				setMedecin((Medecin) getObjectService().getObjectById(
+						idMedecin, "Medecin"));
+			}
+		public void soumettre(){
+			if(isbPv()==true){
+				AjouPv();
+			}
+			else{
+				FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,"Avertissement", "PV non Soumis"));
+			}
+			
+			if(isbRap()==true){
+				AjouRap();
+			}
+			else{
+				FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,"Avertissement", "RAPPORT non Soumis"));
+			}
+			
+			if(isbActMed()==true){
+				AjouActMed();
+			}
+			else{
+				FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,"Avertissement", "Acte Médical non Soumis"));
+			}
+		}
+		
+		//Enregistrement 
+public void AjouPv(){
+			
+			handleFileUploadpv();
+		}
+		
+public void AjouActMed(){
+			facAct.setCodeFacture(getIdGenerateur().getIdFactureActeMed());
+			getObjectService().addObject(facAct);
+			acteMedical.setReferenceActe(getIdGenerateur().getIdActeMed(getSinistre()));
+			acteMedical.setFacture(facAct);
+			acteMedical.setMedecin(getMedecin());
+	
+			getObjectService().addObject(acteMedical);
+			handleFileUploadActeMed();
+		}
+		
+		public void AjouRap(){
+			facPv.setCodeFacture(getIdGenerateur().getIdFactureIntervention(intervention));
+			getObjectService().addObject(facPv);
+			intervention.setRefIntervention(getIdGenerateur().getIdIntervention(getSinistre()));
+			intervention.setTypeRapport("Rapport Intervenant");
+			intervention.setIntervenant(getIntervenant());
+			intervention.setSinistre(sinistre);
+			getObjectService().addObject(intervenant);
+			handleFileUploadRap();
+		}
+		
+		//PDF
+		public void handleFileUploadpv() {
+			byte[] bFile = new byte[102400];
+			if (filePv != null) {
+				try {
+					//copyFile(filePv.getFileName(), filePv.getInputstream());
+					String nom = filePv.getFileName();
+					setNomfichier(nom);
+					System.out.print("file.getFileName()" + filePv.getFileName());
+					System.out.print("nomfichier" + nomfichier);
 
+					if (copyFile(nom, filePv.getInputstream())) {
+						FacesMessage msg = new FacesMessage("Succès téléchargement fichier! ",
+								filePv.getFileName() + " a été téléchargé!");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+						typeRapport = nom;
+					} else {
+						FacesMessage msg = new FacesMessage("Failure! ");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				File fileupload = new File(destination + nomfichier);
+				bFile = filePv.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				creerPdf();
+				File fileupload = new File(destination + "default.pdf");
+				bFile = fileupload.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				typeRapport = "default.pdf";
+			}
+		}
+		
+		public void handleFileUploadRap() {
+			byte[] bFile = new byte[102400];
+			if (fileRap != null) {
+				try {
+					//copyFile(fileRap.getFileName(), fileRap.getInputstream());
+					String nom = fileRap.getFileName();
+					setNomfichier(nom);
+					System.out.print("file.getFileName()" + fileRap.getFileName());
+					System.out.print("nomfichier" + nomfichier);
+
+					if (copyFile(nom, fileRap.getInputstream())) {
+						FacesMessage msg = new FacesMessage("Succès téléchargement fichier! ",
+								fileRap.getFileName() + " a été téléchargé!");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+						typeRapport = nom;
+					} else {
+						FacesMessage msg = new FacesMessage("Failure! ");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				File fileupload = new File(destination + nomfichier);
+				bFile = fileRap.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				creerPdf();
+				File fileupload = new File(destination + "default.pdf");
+				bFile = fileupload.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				typeRapport = "default.pdf";
+			}
+		}
+		
+		public void handleFileUploadActeMed() {
+			byte[] bFile = new byte[102400];
+			if (fileActMed != null) {
+				try {
+					//copyFile(fileActMed.getFileName(), fileActMed.getInputstream());
+					String nom = fileActMed.getFileName();
+					setNomfichier(nom);
+					System.out.print("file.getFileName()" + fileActMed.getFileName());
+					System.out.print("nomfichier" + nomfichier);
+
+					if (copyFile(nom, fileActMed.getInputstream())) {
+						FacesMessage msg = new FacesMessage("Succès téléchargement fichier! ",
+								fileActMed.getFileName() + " a été téléchargé!");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+						typeRapport = nom;
+					} else {
+						FacesMessage msg = new FacesMessage("Failure! ");
+						FacesContext.getCurrentInstance().addMessage(null, msg);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				File fileupload = new File(destination + nomfichier);
+				bFile = fileActMed.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				creerPdf();
+				File fileupload = new File(destination + "default.pdf");
+				bFile = fileupload.toString().getBytes();
+				try {
+					FileInputStream fileInputStream = new FileInputStream(
+							fileupload);
+					// convert file into array of bytes
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				typeRapport = "default.pdf";
+			}
+		}
+		public void creerPdf() {
+			File dir = new File("c:\\upload\\temp\\");
+			dir.mkdirs();
+			try {
+				Document document = new Document();
+				PdfWriter.getInstance(document, new FileOutputStream(FILE));
+				document.open();
+				addMetaData(document);
+				addTitlePage(document);
+				// addContent(document);
+				document.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		private static void addMetaData(Document document) {
+			document.addTitle("My first PDF");
+			document.addSubject("Using iText");
+			document.addKeywords("Java, PDF, iText");
+			document.addAuthor("Lars Vogel");
+			document.addCreator("Lars Vogel");
+		}
+
+		private static void addTitlePage(Document document)
+				throws DocumentException {
+			Paragraph preface = new Paragraph();
+			// We add one empty line
+
+			// Lets write a big header
+			preface.add(new Paragraph(
+					"Vous n'avez pas encore  importer un fichier! ", catFont));
+
+			document.add(preface);
+			// Start a new page
+			document.newPage();
+		}
+		
+		public boolean copyFile(String fileName, InputStream in) {
+
+			try {
+				File dir = new File("c:\\upload\\temp\\");
+				dir.mkdirs();
+
+				// FileWriter newJsp = new FileWriter("c:\\upload\\temp\\test.txt");
+
+				OutputStream out = new FileOutputStream(new File(destination
+						+ fileName));
+
+				// ExternalContext
+				// extContext=FacesContext.getCurrentInstance().getExternalContext();
+				// destination = "//resources//file//" +file.getFileName();
+				// File dir = new File(extContext.getRealPath(destination));
+				// dir.mkdirs();
+				// OutputStream out = new FileOutputStream(new File(destination));
+				int read = 0;
+
+				byte[] bytes = new byte[1024];
+
+				while ((read = in.read(bytes)) != -1) {
+
+					out.write(bytes, 0, read);
+
+				}
+
+				in.close();
+
+				out.flush();
+
+				out.close();
+
+				System.out
+						.println("Un nouveau fichier a été ajouté dans le dossier c:\\upload\\temp\\ !");
+				return true;
+
+			} catch (IOException e) {
+
+				System.out.println(e.getMessage());
+				return false;
+
+			}
+
+		}
+
+		public void upload(UploadedFile file) {
+			try {
+				// copyFile(file.getFileName(), file.getInputstream());
+				String nom = file.getFileName();
+				setNomfichier(nom);
+				System.out.print("file.getFileName()" + file.getFileName());
+				System.out.print("nomfichier" + nomfichier);
+
+				if (copyFile(nom, file.getInputstream())) {
+					FacesMessage msg = new FacesMessage("Succès! ",
+							file.getFileName() + " a été téléchargé!");
+					FacesContext.getCurrentInstance().addMessage(null, msg);
+				} else {
+					FacesMessage msg = new FacesMessage("Failure! ");
+					FacesContext.getCurrentInstance().addMessage(null, msg);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	/*******************************ACCESSEUR recherche****************/
 
 	public Sinistre getSinistre() {
@@ -553,6 +948,272 @@ public class ManagedSinistre implements Serializable {
 
 	public void setDateDeclaration(Date dateDeclaration) {
 		this.dateDeclaration = dateDeclaration;
+	}
+
+
+	public boolean isbPv() {
+		return bPv;
+	}
+
+
+	public void setbPv(boolean bPv) {
+		this.bPv = bPv;
+	}
+
+
+	public boolean isbRap() {
+		return bRap;
+	}
+
+
+	public void setbRap(boolean bRap) {
+		this.bRap = bRap;
+	}
+
+
+	public boolean isbActMed() {
+		return bActMed;
+	}
+
+
+	public void setbActMed(boolean bActMed) {
+		this.bActMed = bActMed;
+	}
+
+
+	public boolean isEtatPv() {
+		return etatPv;
+	}
+
+
+	public void setEtatPv(boolean etatPv) {
+		this.etatPv = etatPv;
+	}
+
+
+	public boolean isEtatRap() {
+		return etatRap;
+	}
+
+
+	public void setEtatRap(boolean etatRap) {
+		this.etatRap = etatRap;
+	}
+
+
+	public boolean isEtatActMed() {
+		return etatActMed;
+	}
+
+
+	public void setEtatActMed(boolean etatActMed) {
+		this.etatActMed = etatActMed;
+	}
+
+
+	public String getIdIntervenant() {
+		return idIntervenant;
+	}
+
+
+	public void setIdIntervenant(String idIntervenant) {
+		this.idIntervenant = idIntervenant;
+	}
+
+
+	public List<SelectItem> getElementsNomInt() {
+		if (elementsNomInt == null) {
+			elementsNomInt = new ArrayList<SelectItem>();
+			try {
+				for (Object obj : getObjectService().getObjects("Intervenant")) {
+       
+					elementsNomInt.add(new SelectItem(((Intervenant) obj)
+							.getCodeIntervenant(), ((Intervenant) obj).getNomIntervenant()));
+
+				}
+			} catch (Exception e) {
+
+			}
+		}
+		return elementsNomInt;
+	}
+
+
+	public void setElementsNomInt(List<SelectItem> elementsNomInt) {
+		this.elementsNomInt = elementsNomInt;
+	}
+
+
+	public List<SelectItem> getElementsNomMed() {
+		if (elementsNomMed == null) {
+			elementsNomMed = new ArrayList<SelectItem>();
+			try {
+				for (Object obj : getObjectService().getObjects("Medecin")) {
+       
+					elementsNomMed.add(new SelectItem(((Medecin) obj)
+							.getCodeMedecin(), ((Medecin) obj).getRaisonSociale()));
+
+				}
+			} catch (Exception e) {
+
+			}
+		}
+		return elementsNomMed;
+	}
+
+
+	public void setElementsNomMed(List<SelectItem> elementsNomMed) {
+		this.elementsNomMed = elementsNomMed;
+	}
+
+
+	public UploadedFile getFilePv() {
+		return filePv;
+	}
+
+
+	public void setFilePv(UploadedFile filePv) {
+		this.filePv = filePv;
+	}
+
+
+	public UploadedFile getFileRap() {
+		return fileRap;
+	}
+
+
+	public void setFileRap(UploadedFile fileRap) {
+		this.fileRap = fileRap;
+	}
+
+
+	public UploadedFile getFileActMed() {
+		return fileActMed;
+	}
+
+
+	public void setFileActMed(UploadedFile fileActMed) {
+		this.fileActMed = fileActMed;
+	}
+
+
+	public UIComponent getButtonadd() {
+		return buttonadd;
+	}
+
+
+	public void setButtonadd(UIComponent buttonadd) {
+		this.buttonadd = buttonadd;
+	}
+
+
+	public Intervention getIntervention() {
+		return intervention;
+	}
+
+
+	public void setIntervention(Intervention intervention) {
+		this.intervention = intervention;
+	}
+
+
+	public Intervenant getIntervenant() {
+		return intervenant;
+	}
+
+
+	public void setIntervenant(Intervenant intervenant) {
+		this.intervenant = intervenant;
+	}
+
+
+	public ActeMedical getActeMedical() {
+		return acteMedical;
+	}
+
+
+	public void setActeMedical(ActeMedical acteMedical) {
+		this.acteMedical = acteMedical;
+	}
+
+
+	public Facture getFacAct() {
+		return facAct;
+	}
+
+
+	public void setFacAct(Facture facAct) {
+		this.facAct = facAct;
+	}
+
+
+	public Facture getFacPv() {
+		return facPv;
+	}
+
+
+	public void setFacPv(Facture facPv) {
+		this.facPv = facPv;
+	}
+
+
+	public Medecin getMedecin() {
+		return medecin;
+	}
+
+
+	public void setMedecin(Medecin medecin) {
+		this.medecin = medecin;
+	}
+
+
+	public String getIdMedecin() {
+		return idMedecin;
+	}
+
+
+	public void setIdMedecin(String idMedecin) {
+		this.idMedecin = idMedecin;
+	}
+
+
+	public static String getFILE() {
+		return FILE;
+	}
+
+
+	public static void setFILE(String fILE) {
+		FILE = fILE;
+	}
+
+
+	public String getDestination() {
+		return destination;
+	}
+
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+
+	public java.lang.String getTypeRapport() {
+		return typeRapport;
+	}
+
+
+	public void setTypeRapport(java.lang.String typeRapport) {
+		this.typeRapport = typeRapport;
+	}
+
+
+	public String getNomfichier() {
+		return nomfichier;
+	}
+
+
+	public void setNomfichier(String nomfichier) {
+		this.nomfichier = nomfichier;
 	}
 
 }
