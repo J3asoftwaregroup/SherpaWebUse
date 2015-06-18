697314411f9eef7fa084b0a13804426a4f215a23
IRENE <IRENE@IRENE-PC>
2015-06-18 11:08:21 +0000

    n

--- src/com/j3a/assurance/managedBean/admin/Categorie1MB.java
+++ src/com/j3a/assurance/managedBean/admin/Categorie1MB.java
@@ -2,6 +2,7 @@ package com.j3a.assurance.managedBean.admin;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.j3a.assurance.model.RcTarif1;
+import com.j3a.assurance.model.RcTarif3;
 import com.j3a.assurance.model.Tarif;
 import com.j3a.assurance.objetService.ObjectService;
 
@@ -21,18 +23,50 @@ public class Categorie1MB implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Autowired
     ObjectService objectService;
-	private RcTarif1 rcTarif1;
-	private Tarif tarif;
+	private RcTarif1 rcTarif1 = new RcTarif1();
+	private Tarif tarif =new Tarif();
+	private  String code;
+	private  String codetar;
+	
+	
+	@PostConstruct
+	public void PostConst(){
+		try {
+			setRcTarif1( (RcTarif1) getObjectService().getObjectById("Rctarif1", "RcTarif1"));
+			setTarif( (Tarif) getObjectService().getObjectById("tarif1", "Tarif"));
+			System.out.println("ok Enregistrement recuperee");
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("non ok !Enregistrement non recuperée");		
+
+		}		
+		
+		}
 	
 	
 	public void addCategorie(){
 	try {
+		code ="Rctarif3";
+		codetar ="tarif3";
+	RcTarif1 rcTarif1Tempon = (RcTarif1) getObjectService().getObjectById(code, "RcTarif1");
+	Tarif tarifTempon = (Tarif) getObjectService().getObjectById(codetar, "Tarif");
+	
+	if((rcTarif1Tempon==null) && (tarifTempon==null)){
 		rcTarif1.setCodeRcTarif1("RcTarif1");	
 		getObjectService().addObject(rcTarif1);	
 		tarif.setCodeTarif("Tarif1");
 		tarif.setRcTarif1(rcTarif1);
+		tarif.setLibelleTarif("Tarif 1");
 		getObjectService().addObject(tarif);	
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Success", "Enregistrement effectué"));
+	}
+	
+	else {
+		getObjectService().updateObject(rcTarif1);
+		getObjectService().updateObject(tarif);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("succes", "La mise à jour a été bien effectuée"));
+	}
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -68,6 +102,26 @@ public class Categorie1MB implements Serializable{
 	}
 	public void setObjectService(ObjectService objectService) {
 		this.objectService = objectService;
+	}
+
+
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public String getCodetar() {
+		return codetar;
+	}
+
+
+	public void setCodetar(String codetar) {
+		this.codetar = codetar;
 	}
 	
 }
